c16f24add575270cea932f8405016bbd79181372
Serg957 <sh.s.e.57@gmail.com>
2024-09-11 19:54:55 +0300

    Add files via upload

--- module_7_4.py
+++ module_7_4.py
@@ -53,19 +53,3 @@ tasks_total = score1 + score2
 time_avg = round((team1_time + team2_time) / tasks_total, 1)
 print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу')
 
-# Комментарии к заданию:
-
-# Переменные challenge_result, tasks_total, time_avg можно задать вручную или рассчитать. Например, для challenge_result:
-# if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
-# result = ‘Победа команды Мастера кода!’
-# elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
-# result = ‘Победа команды Волшебники Данных!’ else:
-# result = ‘Ничья!’
-#
-# Пример входных данных
-# team1_num = 6 team2_num = 6
-# score1 = 40
-# score2 = 42
-# team1_time = 1552.512 team2_time = 2153.31451 tasks_total = 82
-# time_avg = 45.2
-# challenge_result = 'Победа
